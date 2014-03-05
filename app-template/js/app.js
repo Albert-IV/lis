3446f9ac6827c756cd86797b6bd9e56d0b652466
droppedonjapan <dropped.on.japan@gmail.com>
2014-03-04 19:35:46 -0600

    Moved to backbone, undid everything. OHGOD EVERYTHING IS BAD

--- app-template/js/app.js
+++ app-template/js/app.js
@@ -1,25 +1,12 @@
-/*global app, $on */
-(function () {
+/*global $ */
+/*jshint unused:false */
+var app = app || {};
+var ENTER_KEY = 13;
+var ESC_KEY = 27;
+
+$(function () {
 	'use strict';
 
-	/**
-	 * Sets up a brand new Todo list.
-	 *
-	 * @param {string} name The name of your new to do list.
-	 */
-	function Todo(name) {
-		this.storage = new app.Store(name);
-		this.model = new app.Model(this.storage);
-		this.template = new app.Template();
-		this.view = new app.View(this.template);
-		this.controller = new app.Controller(this.model, this.view);
-	}
-
-	var todo = new Todo('todos-vanillajs');
-
-	function setView() {
-		todo.controller.setView(document.location.hash);
-	}
-	$on(window, 'load', setView);
-	$on(window, 'hashchange', setView);
-})();
+	// kick things off by creating the `App`
+	new app.AppView();
+});
